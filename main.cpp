c615d500a18857dc58d718e64c3cb9dea777a031
justinracca <justinracca07@gmail.com>
2025-09-24 11:08:10 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "  *   .       *          .          .        *       .        .\n";
+    std::cout << "             .       *          .       .         .           .\n";
+    std::cout << ".        *      .        .       *        .          .       *.\n";
+    std::cout << " .    .       .        .       .         .        .    *      .\n";
+    std::cout << "JJJJJJJJJ  UUU    UUU  SSSSSSSS  TTTTTTTT  IIIIIIII  NN      NN\n";
+    std::cout << "JJJJJJJJJ  UUU    UUU  SS    SS  TT TT TT  II II II  NNN     NN\n";
+    std::cout << "    JJJJ   UUU    UUU  SS           TT        II     NNNN    NN\n";
+    std::cout << "    JJJJ   UUU    UUU   SSSSSS      TT        II     NN NNN  NN\n";
+    std::cout << "    JJJJ   UUU    UUU   SSSSSS      TT        II     NN  NNN NN\n";
+    std::cout << "JJ  JJJJ   UUU    UUU        SS     TT        II     NN    NNNN\n";
+    std::cout << "JJJJJJJJ    UUU  UUUU  SS    SS     TT     II II II  NN     NNN\n";
+    std::cout << " JJJJJ       UUUUUU    SSSSSSSS     TT     IIIIIIII  NN      NN\n";
+       std::cout << "  *   .       *          .        .        *       .        \n";
+    std::cout << "             .       *          .       .         .           .\n";
+    std::cout << ".        *      .        .       *        .        .         *.\n";
+    std::cout << " .    .       .        .       .         .        .    *      .\n";
+    return 0; 
+}
